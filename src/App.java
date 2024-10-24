59bb164c4e481206e6372d73dfb360aaeed0f1bb
Julian-25 <jjpedrazab@ufpso.edu.co>
2024-10-24 17:49:28 -0500

    Juego terminado en un 80%

--- src/App.java
+++ src/App.java
@@ -10,13 +10,16 @@ class Personaje {
     int ataque_especial;
     int curar;
     Random random;
+    int curacionLimite;
+    boolean transformado;
+    int segunda_fase;
 
     // Definir constructor
     public Personaje(
             String nombre,
             int fuerza,
             int velocidad,
-            int vida_hp, int ataque_especial, int curar) {
+            int vida_hp, int ataque_especial, int curar, boolean transformado, int segunda_fase) {
         this.nombre = nombre;
         this.fuerza = fuerza;
         this.velocidad = velocidad;
@@ -24,38 +27,57 @@ class Personaje {
         this.curar = curar;
         this.ataque_especial = ataque_especial;
         this.random = new Random();
+        this.curacionLimite = 0;
+        this.transformado = transformado;
+        this.segunda_fase = segunda_fase;
+
         }
     public void recibirDaño(double daño){
-        daño = fuerza;
-        vida_hp -= daño;
-        System.out.println(nombre +" ataca a ");
+        vida_hp -= fuerza;
+        System.out.println(nombre +" recibe daño de " +daño +" puntos de daño");
     }
     public void atacar (Personaje npc){
+        System.out.println(nombre +" ataca con una fuerza de: " +fuerza);
         npc.recibirDaño(fuerza);
-        System.out.println(nombre +" recibe un ataque de " +fuerza);
     }
 
     public void curar() {
-        int curacion = 90; 
-        vida_hp += curacion;
-        System.out.println(nombre + " se ha curado " + curacion + " puntos de vida. Vida actual: " + vida_hp);
+        if (curacionLimite < 3) {
+            int curacion = 90; 
+            vida_hp += curacion;
+            System.out.println(nombre + " se ha curado " + curacion + " puntos de vida. Vida actual: " + vida_hp);
+            
+        }else{
+            System.out.println("Ya haz alcanzado el limtei de curaciones");
+        }
     }
     
     public void ataqueEspecial(Personaje npc){
-        System.out.println(nombre +" ha utilizado su ataque especial: " +ataque_especial);
+        System.out.println(nombre +" ha utilizado su ataque especial " +ataque_especial);
         npc.recibirDaño(ataque_especial);
+    }
+
+    public void transformar() {
+        if (!transformado) {
+            
+            fuerza += segunda_fase ; 
+            transformado = true;
+            System.out.println(nombre + " se ha transformado! Fuerza amuentada.");
+        } else {
+            System.out.println(nombre + " ya está transformado.");
+        }
     }
 
     public void realizarAcciones(Personaje npc){
         int accion = random.nextInt(3);
         switch (accion) {
-            case 1:
+            case 0:
             atacar(npc);
             break;
-            case 2:
+            case 1:
             curar();;
             break;
-            case 3:
+            case 2:
             ataqueEspecial(npc);
             break;
             default:
@@ -71,7 +93,7 @@ class Goku extends Personaje {
     // definir el constructor
     public Goku(){
 
-            super("Goku", 80, 350, 250,75,100);
+            super("Goku", 80, 350, 250,75,100, false, 20 );
     }
     @Override
     public void recibirDaño(double daño) {
@@ -84,7 +106,7 @@ class Vegeta extends Personaje {
     // definir el constructor
     public Vegeta(){
 
-            super("Vegeta", 75, 320, 220, 80,90);
+            super("Vegeta", 75, 320, 220, 80,90, false, 18);
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -97,7 +119,7 @@ class Picoro extends Personaje {
     // definir el constructor
     public Picoro (){
 
-            super("Picoro", 55, 150, 200, 75,80);
+            super("Picoro", 55, 150, 200, 75,80, false, 11);
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -106,14 +128,17 @@ class Picoro extends Personaje {
 }
 
 class Gohan extends Personaje {
-    // definir el constructor
-    public Gohan (){
-
-            super("Gohan", 60, 250, 190, 55,70);
+    public Gohan() {
+        super("Gohan", 60, 250, 190, 55, 70, false, 7);
     }
-    public void recibirDaño(double daño) {
-        System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
-        super.recibirDaño(daño); 
+
+    public void transformar() {
+        if (vida_hp > 0) {
+            fuerza += 30; // Aumentar fuerza adicional para Super Saiyajin 2
+            System.out.println(nombre + " se ha transformado en Super Saiyajin 2! Fuerza aumentada.");
+        } else {
+            System.out.println(nombre + " no puede transformarse en estado crítico.");
+        }
     }
 }
 
@@ -121,7 +146,7 @@ class Krillin extends Personaje {
     // definir el constructor
     public Krillin (){
 
-            super("Krillin", 45, 150, 180, 45,50);
+            super("Krillin", 45, 150, 180, 45,50, false, 6);
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -133,7 +158,7 @@ class Trunks extends Personaje {
     // definir el constructor
     public Trunks (){
 
-            super("Trunks", 65, 300, 200, 60,70);
+            super("Trunks", 65, 300, 200, 60,70, false, 20);
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -145,7 +170,7 @@ class Ten extends Personaje {
     // definir el constructor
     public Ten (){
 
-            super("Ten", 50, 150, 150, 35,40);
+            super("Ten", 50, 150, 150, 35,40, false, 7);
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -157,7 +182,7 @@ class Cell extends Personaje {
     // definir el constructor
     public Cell (){
 
-            super("Cell", 80, 300, 800, 80,68);
+            super("Cell", 80, 300, 800, 80,68, false, 0 );
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -168,7 +193,7 @@ class Freezer extends Personaje {
     // definir el constructor
     public Freezer (){
 
-            super("Freezer", 65, 250, 750,70, 70);
+            super("Freezer", 65, 250, 750,70, 70, false, 0);
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -179,7 +204,7 @@ class Numero_17 extends Personaje {
     // definir el constructor
     public Numero_17 (){
 
-            super("Numero 17", 68, 295, 770, 75,60);
+            super("Numero 17", 68, 295, 770, 75,60, false, 0);
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -190,7 +215,7 @@ class Numero_18 extends Personaje {
     // definir el constructor
     public Numero_18 (){
 
-            super("Numero 18", 70, 300, 770, 76,60);
+            super("Numero 18", 70, 300, 770, 76,60, false, 0);
     }
     public void recibirDaño(double daño) {
         System.out.println(nombre + " es un guerrero fuerte, reduce el daño recibido.");
@@ -237,6 +262,7 @@ public class App {
             System.out.println("El personaje elegido será Gohan");
             System.out.println("Sus estadisticas y habilidades son: 190 HP, 250 de velocidad, defender( dara un 35% de defensa al daño recibido durante ese turno) , Super sayayin(gohan  aumentará el poder de sus golpes de sus golpes base en un 20% durante 5 turnos turnos) , semilla del hermitaño curará 25 o hasta 40% (La curacion dependerá de la vida del personaje) y su ataque especial será  Kame hame ha(el daño total 55 dependiendo si el super sayayin está activo están activas entonces sera de 70)");
             System.out.println("[FACTOR SECRETO] luego de 10 turnos gohan podrá activar el super sayayin 2, esto hará que el Kame hame ha  baje un total de (150)");
+            break;
             case 6:
             jugador = new Ten();
             System.out.println("El personaje elegido será Ten shin han");
@@ -282,13 +308,21 @@ public class App {
                 break;
         }
 
+        int ronda = 1; // Inicializar la ronda
+
         while (jugador.vida_hp > 0 && npc.vida_hp > 0) {
-            int curacionLimite = 0;
             System.out.println("\nTurno del jugador:");
+            System.out.println("Ronda: " + ronda);
             System.out.println("1. Atacar");
             System.out.println("2. Curarse");
             System.out.println("3. Ataque especial");
-            System.out.print("Elija su acción (1-3): ");
+            if (ronda % 3 == 0) {
+                System.out.println("4. Transformarse"); // Opción de transformación
+            }
+            if (ronda == 10 && jugador instanceof Gohan) {
+                System.out.println("4. Transformarse en Super Saiyajin 2");
+            }
+            System.out.print("Elija su acción (1-4): ");
             int accionJugador = scanner.nextInt();
 
             switch (accionJugador) {
@@ -296,16 +330,17 @@ public class App {
                     jugador.atacar(npc);
                     break;
                 case 2:
-                if (curacionLimite < 3) {
                     jugador.curar();
-                  
-                     }
-
-                curacionLimite++;
-
                     break;
                 case 3:
                     jugador.ataqueEspecial(npc);
+                    break;
+                case 4:
+                    if (ronda % 3 == 0) {
+                        jugador.transformar();
+                    } else {
+                        System.out.println("No puedes transformarte en esta ronda.");
+                    }
                     break;
                 default:
                     System.out.println("Acción no válida.");
@@ -323,11 +358,11 @@ public class App {
                 System.out.println(jugador.nombre + " ha sido derrotado. ¡Game Over!");
                 break;
             }
+
+            
+            ronda++; // Incrementar la ronda
         }
 
-
-
-
-
+        scanner.close();
     }
-}
+    }
